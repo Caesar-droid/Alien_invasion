4953f9e12f25be53ce435cd31616c42ad9351259
domie <dominictoroitichh@gmail.com>
2021-03-10 13:01:19 +0300

    adding a helper method

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,16 +17,18 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            #Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.bliteme()
-            #make the most recently drawn screen visible.
-            pygame.display.flip() 
+            self._update_screen()
     def _check_events(self):
         #watch for the keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+    def _update_screen(self):
+        #Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.bliteme()
+        #make the most recently drawn screen visible.
+        pygame.display.flip() 
 if __name__ == '__main__':
     #make a game instance, and run the game
     ai=AlienInvasion()
