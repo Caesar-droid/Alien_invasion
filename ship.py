ae5c5136e4ef4612f8f08ca75b6bea89797b7347
domie <dominictoroitichh@gmail.com>
2021-03-15 22:05:41 +0300

    correcting the ships self.x coordinates

--- ship.py
+++ ship.py
@@ -23,9 +23,9 @@ class Ship:
         """Update the ship's position based on the movement flag."""
         #update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.rect.x += self.settings.ship_speed
+            self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
-            self.rect.x -= self.settings.ship_speed
+            self.x -= self.settings.ship_speed
         #update rect object from self.x.
         self.rect.x = self.x
     def bliteme(self):
