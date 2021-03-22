c9b6ed6343c918ae89376ce370343859cda34290
domie <dominictoroitichh@gmail.com>
2021-03-22 12:23:39 +0300

    moving the aliens right

--- alien.py
+++ alien.py
@@ -6,6 +6,7 @@ class Alien(Sprite):
     def __init__(self,ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         #load the alien image and set its rect attribute
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
@@ -16,5 +17,8 @@ class Alien(Sprite):
         
         #store the alien's exact horizontal position.
         self.x = float(self.rect.x)
-        
+    def update(self):
+        """Move the alien to the right."""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x  
         
--- alien_invasion.py
+++ alien_invasion.py
@@ -27,7 +27,8 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
-            self._create_fleet()
+            # self._create_fleet()
+            self._update_aliens()
             self._update_screen()
             
     def _check_events(self):
@@ -69,6 +70,9 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet) 
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
     def _create_fleet(self):
         """Create the fleet of aliens."""
         #create an alien and find the number of aliens in a row.
