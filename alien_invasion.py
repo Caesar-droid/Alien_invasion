43af7b949d03c32765602b12d58971c7a8b5c9d7
domie <dominictoroitichh@gmail.com>
2021-03-23 22:08:36 +0300

    repopulating the fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -77,6 +77,10 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets , self.aliens , True, True
         )
+        if not self.aliens:
+            #destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
     def _update_aliens(self):
         """Check if the fleet is at an edge,
         Update the positions of all aliens in the fleet."""
