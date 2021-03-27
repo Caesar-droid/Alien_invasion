0cbbbba8b271ce388b9122c46f89a70229027966
domie <dominictoroitichh@gmail.com>
2021-03-27 21:19:49 +0300

    minor error

--- alien_invasion.py
+++ alien_invasion.py
@@ -108,7 +108,7 @@ class AlienInvasion:
         alien_width , alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
-        #Dtermine the number of rows of aliens that fit on the screen.
+        #Determine the number of rows of aliens that fit on the screen.
         ship_height = self.ship.rect.height
         available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
         number_rows =  available_space_y // (alien_height)
